99d95394ca30568cd281cc91cd2088bc21bcecd0
juanidiaz <jdi@idiaz.ca>
2018-12-27 23:39:58 -0500

    Now players are tracked also winner of first match

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -6,6 +6,13 @@
 
 //      STRINGS/CHAR
 var userId = ''; // User UD when connecting to database
+var p1name = '';
+var p1choice = '';
+var p1id = '';
+var p2name = '';
+var p2choice = '';
+var p2id = '';
+
 
 //      NUMBER/INTEGER
 var viewers = 0; // How many connections
@@ -28,7 +35,6 @@ var oponent = new playerInfo('', ''); // Contains OPONENT payer info
 
 // ------------------------------------------------------------
 
-
 $(document).ready(function () {
 
     var config = {
@@ -42,15 +48,89 @@ $(document).ready(function () {
 
     firebase.initializeApp(config);
 
-    // Create a variable to reference the database.
+    // Create references to the database
     var database = firebase.database();
-
     var gameStats = database.ref("/rpm_multiuser/gameStats");
-
-    // All connections will be stored in this directory.
+    var playerInfo = database.ref("/rpm_multiuser/playerInfo");
     var connectionsRef = database.ref("/rpm_multiuser/connections");
-
     var connectedRef = database.ref(".info/connected");
+
+    // ----------------------------------------------
+
+    // Look for viewers in 'player' mode
+    function checkFor(mode) {
+
+        let modeRef = firebase.database().ref('/rpm_multiuser');
+
+        modeRef.child('connections').orderByChild('mode').equalTo(mode).on("value", function (snapshot) {
+
+            // Number of players are as many connections in 'player' mode
+            players = parseInt(snapshot.numChildren());
+
+            // Clear the variables based on # of players
+            switch (players) {
+                case 0:
+                    p1name = '';
+                    p1choice = '';
+                    p1id = '';
+                    p2name = '';
+                    p2choice = '';
+                    p2id = '';
+                    break;
+
+                case 1:
+                    p2name = '';
+                    p2choice = '';
+                    p2id = '';
+                    break;
+            }
+
+            // Get the id, name anc choice of players and save localy
+            var i = 1;
+            snapshot.forEach(function (data) {
+                switch (i) {
+                    case 1:
+                        if (data.val().name) {p1name = data.val().name;} else {p1name = ''};
+                        if (data.val().choice) {p1choice = data.val().choice;} else {p1choice = ''};
+                        if (data.key) {p1id = data.key;} else { p1id = ''};
+                        break;
+
+                    case 2:
+                        if (data.val().name) {p2name = data.val().name;} else { p2name = ''};
+                        if (data.val().choice) {p2choice = data.val().choice;} else {p2choice = ''};
+                        if (data.key) {p2id = data.key;} else {p2id = ''};
+                        break;
+                }
+
+                if (data.key !== userId) {
+                    oponent.name = data.val().name;
+                    oponent.choice = data.val().choice;
+                }
+
+                i++;
+            });
+
+            // Update the number of players in the database
+            gameStats.update({
+                "players": players
+            });
+
+            // Now update these values in the database
+            playerInfo.update({
+                "p1name": p1name,
+                "p1choice": p1choice,
+                "p1id": p1id,
+                "p2name": p2name,
+                "p2choice": p2choice,
+                "p2id": p2id
+            });
+
+        });
+
+    }
+
+
+    // ----------------------------------------------
 
     function updateScreen() {
 
@@ -58,11 +138,11 @@ $(document).ready(function () {
             $("#user").hide();
             $("#game").show();
             $("#viewerStat").hide();
-        } else if(players < 2) {
+        } else if (players < 2) {
             $("#user").show();
             $("#game").hide();
             $("#viewerStat").hide();
-        }else{
+        } else {
             $("#user").hide();
             $("#game").hide();
             $("#viewerStat").show();
@@ -73,7 +153,7 @@ $(document).ready(function () {
     }
 
     $(".choice").on("click", function () {
-        if(choiceDone){
+        if (choiceDone) {
             return;
         }
         $(".choice").addClass("disabled");
@@ -108,20 +188,60 @@ $(document).ready(function () {
             // Remove user from the connection list when they disconnect.
             con.onDisconnect().remove();
         }
+
+        checkFor('player');
+
     });
+
+    function whoWin(p1, p2) {
+        console.log("Calculating winers");
+        console.log("Choice 1: " + p1);
+        console.log("Choice 2: " + p2);
+
+
+        if ((p1 === "rock") || (p1 === "paper") || (p1 === "scissors")) {
+
+            if ((p1 === "rock" && p2 === "scissors") ||
+              (p1 === "scissors" && p2 === "paper") ||
+              (p1 === "paper" && p2 === "rock")) {
+            //   wins++;
+            console.log("WINNER: P1");
+
+            } else if (p1 === p2) {
+            //   ties++;
+            console.log("its a tie");
+
+            } else {
+            //   losses++;
+            console.log("WINNER: P2");
+
+            }
+
+        }
+
+    }
+
+    playerInfo.on("value", function (snap) {
+
+        if(snap.val().p1choice && snap.val().p2choice){
+            whoWin(snap.val().p1choice, snap.val().p2choice);
+        }
+        
+    });
+
+
+
 
     connectionsRef.on('value', function (snap) {
 
         viewers = parseInt(snap.numChildren());
-
-        // Someone has connected!
-        //console.log("Viewers: " + viewers);
 
         // Update the number of viewers in the database
         gameStats.update({
             "viewers": viewers
         });
 
+        checkFor('player');
         updateScreen();
     })
 
@@ -133,7 +253,7 @@ $(document).ready(function () {
         updateScreen();
     })
 
-    $("#play-bid").on("click", function (event) {
+    $("#play").on("click", function (event) {
         event.preventDefault();
 
         // Escaping if no name was given
@@ -152,14 +272,6 @@ $(document).ready(function () {
         connectionsRef.child(userId).update({
             "mode": "player",
             "name": player.name
-        });
-
-        // Increase the number of players
-        players++;
-
-        // Update the number of players in the database
-        gameStats.update({
-            "players": players
         });
 
     });
